c06a303e8be6f23b4e9a1d7b8cba1a05a633fda4
Michael Phelps <michael.phelps@cpcc.edu>
2020-09-06 17:06:25 -0400

    Make tests safer and add test for Java compilation

--- constructor/test_main.py
+++ constructor/test_main.py
@@ -1,5 +1,7 @@
+import os
 import sys
 import json
+import subprocess
 from importlib.util import spec_from_loader, module_from_spec
 from unittest import TestCase
 
@@ -26,35 +28,54 @@ SIMPLE_TEST_JSON_EXPECTED_CLASSES = ("Simple", "Researcher", "Relative")
 class TestMetaClassSimple(TestCase):
     def setUp(self) -> None:
         self.test_json = SIMPLE_TEST_JSON
-        self.simple_meta_class = MetaClass.from_json("Simple", self.test_json)
+        self.class_name = "Simple"
+        self.meta_class = MetaClass.from_json(self.class_name, self.test_json)
 
     def tearDown(self):
         cleanup()  # TODO: This should NOT be necessary...
 
     def test_python_compiles(self):
-        source = self.simple_meta_class.generate_python()
+        source = self.meta_class.generate_python()
         compile(source, "simple.py", 'exec')
 
     def test_python_has_expected_classes(self):
-        source = self.simple_meta_class.generate_python()
+        source = self.meta_class.generate_python()
         compiled = compile(source, "simple.py", 'exec')
         for expected_class in SIMPLE_TEST_JSON_EXPECTED_CLASSES:
-            assert expected_class in compiled.co_names, f"Output ({compiled.co_names}) is missing the {expected_class} class"
+            self.assertIn(expected_class, compiled.co_names,
+                          f"Output ({compiled.co_names}) is missing the {expected_class} class")
 
     def test_python_to_json_output_matches_original_structure(self):
         # Setup. TODO: Abstract this
-        source = self.simple_meta_class.generate_python()
         module_name = 'test_module'
-        spec = spec_from_loader(module_name, loader=None)
-        module = module_from_spec(spec)
-        exec(source, module.__dict__)
-        sys.modules[module_name] = module
+        try:
+            source = self.meta_class.generate_python()
+            spec = spec_from_loader(module_name, loader=None)
+            module = module_from_spec(spec)
+            exec(source, module.__dict__)
+            sys.modules[module_name] = module
 
-        # Actual test
-        print(source)
-        simple = module.Simple.from_json(self.test_json)
-        test_json = simple.to_json()
-        assert json.loads(test_json) == json.loads(self.test_json)
+            # Actual test
+            simple = module.Simple.from_json(self.test_json)
+            test_json = simple.to_json()
+            self.assertEqual(json.loads(test_json), json.loads(self.test_json))
+        finally:
+            # Teardown. TODO: Abstract this
+            del sys.modules[module_name]
 
-        # Teardown. TODO: Abstract this
-        del sys.modules[module_name]
+    def test_java_compiles(self):
+        # Setup. TODO: Abstract this
+        javaname = f"{self.class_name}.java"
+        javacname = f"{self.class_name}.javac"
+        try:
+            source = self.meta_class.generate_java()
+            with open(javaname, "w") as f:
+                f.write(source)
+
+            # Actual test
+            subprocess.check_output(['javac', javaname])
+        finally:
+            # Teardown. TODO: Abstract this
+            for fp in (javaname, javacname):
+                if os.path.exists(fp):
+                    os.remove(fp)
